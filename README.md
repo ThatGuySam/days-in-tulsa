e585696786bf9731134aad899bd38c0adb18d812
Sam Carlton <ThatGuySam@users.noreply.github.com>
2021-04-25 10:55:29 -0500

    Add html and js

---
+++ README.md
@@ -0,0 +1,5 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ThatGuySam/pen/xxgMyer](https://codepen.io/ThatGuySam/pen/xxgMyer).
+
+
---
+++ script.js
@@ -0,0 +1,12 @@
+const days = document.querySelector('#days')
+
+const revivalStart = new Date('Febuary 2, 2021 07:00:00')
+
+const timeDifference = Date.now() - revivalStart.getTime()
+  
+// Calculate the no. of days between two dates
+const daysDifference = timeDifference / (1000 * 3600 * 24)
+
+console.log('r', revivalStart)
+  
+days.innerHTML = Math.ceil(daysDifference)
